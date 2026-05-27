0f42b7a605dd30ddcae619a9c3852cc4f8234ac6
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-27 12:48:01 -0300

    feat: criando models de clientes

--- src/models/models.py
+++ src/models/models.py
@@ -1,14 +1,20 @@
 # Arquivo de configuracao Models
 
-from sqlalchemy import Column, Float, Integer, String
-from connection.connection import Base
+from sqlalchemy import Float, Integer, String
+from src.connection.connection import Base
+from sqlalchemy.orm import  DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+
 
 class Cliente(Base):
     __tablename__ = "clientes"
 
-    id = Column(Integer, primary_key=True, index=True)
-    cliente_name = Column(String, index=True)
-    cliente_email = Column(String, unique=True, index=True)
-    tipo_solicitacao = Column(String)
-    valor_patrimonio = Column(Float)
-    status = Column(String, default="Pendente")
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    cliente_name: Mapped[str] = mapped_column(String, index=True)
+    cliente_email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    tipo_solicitacao: Mapped[str] = mapped_column(String)
+    valor_patrimonio: Mapped[float] = mapped_column(Float)
+    status: Mapped[str] = mapped_column(String, default="Atualização cadastral")
