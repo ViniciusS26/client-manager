faf45515ea09e5fd356dc0bc00fe360900062ec3
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-27 12:46:03 -0300

    feat: removendo função de get de dentro do schema

--- src/graphql/schema.py
+++ src/graphql/schema.py
@@ -1,5 +1,7 @@
 import typing
 import strawberry
+
+
 """
 {
   "cliente_nome": "João Silva",
@@ -16,15 +18,10 @@ class Cliente:
     cliente_email: str
     tipo_solicitacao: str
     valor_patrimonio: float
+    status: str
 
 
-def get_cliente() -> Cliente:
-    return Cliente(
-        cliente_name="João Silva",
-        cliente_email="joao.silva@example.com",
-        tipo_solicitacao="Atualização cadastral",
-        valor_patrimonio=250000.0
-    )
+
 
 
 
