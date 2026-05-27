2046b1160c07ca835313e598e68f03e77f34a647
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-27 12:43:41 -0300

    fix: alterando configuração de acesso ao banco de dados postgres

--- src/connection/connection.py
+++ src/connection/connection.py
@@ -1,6 +1,6 @@
 import os
 from unittest.mock import Base
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 """
@@ -8,6 +8,22 @@ Arquivo de configuração da conexão com o banco de dados usando SQLAlchemy
 """
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Configura o construtor de sessões assíncronas
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db_session():
+    """Função para obter uma sessão de banco de dados, garantindo que seja fechada após o uso"""
+    async with AsyncSessionLocal() as session:
+        yield session
+
 Base = declarative_base()
